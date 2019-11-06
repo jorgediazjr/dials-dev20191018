c444dbcfdc57c25c17a42a891d12f9e02e7955de
Jorge Diaz <jorge7soccer@gmail.com>
2019-11-06 11:13:26 -0500

    Added print("{[0]:.2f}\t{[1]:.2f}\t{[2]:.2f}".format(centroids[i]))

--- modules/dials/util/close_spots.py
+++ modules/dials/util/close_spots.py
@@ -52,5 +52,5 @@ def main(reflections):
 
     print("JAD7: centroids = {}".format(centroids))
     for i in range(len(centroids)):
-        print(" {} ".format(centroids[i]))
+        print("{[0]:.2f}\t{[1]:.2f}\t{[2]:.2f}".format(centroids[i]))
     print("JAD7: intensities = {}".format(intensities))
