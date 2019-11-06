fe6480d9be8c5486a37e17252ab11d81e7cbbe2f
Jorge Diaz <jorge7soccer@gmail.com>
2019-11-06 11:04:28 -0500

    Added 2 print statements for centroid and intensities.

--- modules/dials/util/close_spots.py
+++ modules/dials/util/close_spots.py
@@ -48,51 +48,8 @@ def main(reflections):
         xyzobs.px.value
         xyzobs.px.variance
     '''
+    centroids = result['xyzobs.px.value']
+    intensities = result['intensity.sum.value']
 
-    '''
-    print("JAD7: close_spots ==> result[bbox] = {}".format(result['bbox']))
-    print("JAD7: close_spots ==> dir(result[bbox]) = {}".format(dir(result['bbox'])))
-    print("JAD7: close_spots ==> result[bbox].__dict__ = {}".format(result['bbox'].__dict__))
-    print("JAD7: close_spots ==> result[bbox].__doc__ = {}".format(result['bbox'].__doc__))
-    print("JAD7: close_spots ==> result[bbox].as_int() = {}".format(result['bbox'].as_int()))
-    print("JAD7: close_spots ==> dir(result[bbox].as_int()) = {}".format(dir(result['bbox'].as_int())))
-    print("JAD7: close_spots ==> dir(result[bbox].as_int().as_numpy_array()) = {}".format(dir(result['bbox'].as_int().as_numpy_array())))
-    print("JAD7: close_spots ==> result[bbox].as_int().as_numpy_array() = {}".format(result['bbox'].as_int().as_numpy_array()))
-
-    index = 0
-    while index < len( result['bbox'].as_int().as_numpy_array() ):
-        print("{}\t{}\t{}\t{}\t{}\t{}".format(result['bbox'].as_int().as_numpy_array()[index],
-                                            result['bbox'].as_int().as_numpy_array()[index+1],
-                                            result['bbox'].as_int().as_numpy_array()[index+2],
-                                            result['bbox'].as_int().as_numpy_array()[index+3],
-                                            result['bbox'].as_int().as_numpy_array()[index+4],
-                                            result['bbox'].as_int().as_numpy_array()[index+5])
-             )
-        index += 6 
-
-    '''
-
-    '''
-    print("JAD7: result[flags] ==> {}".format(result['flags']))
-    print("JAD7: dir(result[flags]) ==> {}".format(dir(result['flags'])))
-    print("JAD7: result[flags].accessor() ==> {}".format(result['flags'].accessor()))
-    print("JAD7: dir(result[flags].accessor()) ==> {}".format(dir(result['flags'].accessor())))
-    print("JAD7: result[flags].accessor().show_summary() ==> {}".format(result['flags'].accessor().show_summary()))
-    print("JAD7: result[flags].accessor().all() ==> {}".format(result['flags'].accessor().all()))
-    print("JAD7: dir(result[flags].accessor().all()) ==> {}".format(dir(result['flags'].accessor().all())))
-    print("JAD7: result[flags].accessor().all()[0] ==> {}".format(result['flags'].accessor().all()[0]))
-
-    print("JAD7: result[flags].all() ==> {}".format(result['flags'].all()))
-    print("JAD7: dir(result[flags].all()[0]) ==> {}".format(dir(result['flags'].all()[0])))
-    print("JAD7: result[flags].all().__doc__ ==> {}".format(result['flags'].all().__doc__))
-    '''
-
-    '''
-    print("JAD7: find_spots - result.flags() => {}".format(result.flags()))
-    print("JAD7: find_spots - dict(result) => {}".format(dict(result)))
-    print("JAD7: find_spots - result[xyzobs.px.value] => {}".format(result['xyzobs.px.value']))
-    print("JAD7: find_spots - dir(result[xyzobs.px.value]) => {}".format(dir(result['xyzobs.px.value'])))
-    print("JAD7: find_spots - str(result[xyzobs.px.value]) => {}".format(str(result['xyzobs.px.value'])))
-    print("JAD7: find_spots - dict(result[xyzobs.px.value][0]) => {}".format(dict(result['xyzobs.px.value'][0])))
-    '''
-
+    print("JAD7: centroids = {}".format(centroids))
+    print("JAD7: intensities = {}".format(intensities))
