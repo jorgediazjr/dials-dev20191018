e0b31d25343e7ee8fa7be6553130f9d5449d595e
Jorge Diaz <jorge7soccer@gmail.com>
2019-11-08 11:49:38 -0500

    Added two functions -> add_index_to_dict(), euclidean_distance().

--- modules/dials/util/close_spots.py
+++ modules/dials/util/close_spots.py
@@ -28,7 +28,52 @@ def get_x_y_pairs(result):
 
     return xy_pairs
 
-def find_close_spots(result):
+
+def order_xy_pairs(xy_pairs):
+    return collections.OrderedDict(sorted(xy_pairs.items()))
+
+
+def add_index_to_pairs(ordered_pairs):
+    new_dict = {}
+    for i, x in enumerate(ordered_pairs):
+        new_dict[i] = {x: ordered_pairs[x]}
+    return new_dict
+
+
+def euclidean_distance(ordered_pairs, distance=0.5):
+    close_pairs = []        # these are pairs of points that are close together
+    closest_pairs = dict()  # this has x,y values that are closest
+    midpoints = dict()      # the midpoints between the close pairs
+    for index in ordered_pairs:
+        for x_coord in ordered_pairs[index]:
+            for y_coord in ordered_pairs[index][x_coord]:
+                x1 = x_coord
+                y1 = y_coord
+                point_a = [x1, y1]
+                current = 0
+                while current < len(ordered_pairs):
+                    if current != index:
+                        x2 = list(ordered_pairs[current].keys())[0]
+                        for _y in ordered_pairs[current][x2]:
+                            y2 = _y
+                            point_b = [x2, y2]
+                            dist = find_distance(point_a, point_b)
+                            distance = units_distance(point_a, point_b)
+                            if dist <= distance:
+                                pair = [point_a, point_b]
+                                print("{}\t<-- {:.2f} -->\t{}\tWITHIN {}".format(point_a, dist, point_b, distance))
+                                close_pairs.append(pair)
+                                closest_pairs[point_b[0]] = [point_b[1]]
+                                # save the midpoints of the close pairs
+                                midpoint_x = (point_a[0] + point_b[0]) / 2
+                                midpoint_y = (point_a[1] + point_b[1]) / 2
+                                midpoints[midpoint_x] = midpoint_y
+                        current += 1
+                    else:
+                        current += 1
+    return close_pairs, midpoints, closest_pairs
+
+def find_close_spots(x_y_pairs):
 
     pass
 
@@ -38,6 +83,17 @@ def main(reflections):
     result = read_in_file()
     x_y_pairs = get_x_y_pairs(result)
     print("JAD7: Finished get_xy_pairs()")
+
+    ordered_pairs = order_xy_pairs(x_y_pairs)
+    ordered_pairs = add_index_to_pairs(ordered_pairs)
+    close_pairs, midpoints, closest_pairs = euclidean_distance(ordered_pairs)
+    
+
+
+
+
+
+
 
     print("JAD7: close_spots - result => {}".format(result))
     for item in dir(result):
