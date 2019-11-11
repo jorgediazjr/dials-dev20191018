7e020cf62334be10ba3ab46c7eb786b06ea04488
Jorge Diaz <jorge7soccer@gmail.com>
2019-11-11 13:52:51 -0500

    Added -> dict(reflections.keys()) .

--- modules/dials/algorithms/spot_finding/finder.py
+++ modules/dials/algorithms/spot_finding/finder.py
@@ -749,7 +749,7 @@ class SpotFinder(object):
         reflections = flex.reflection_table()
         print("reflections.keys() = {}".format(reflections.keys()))
         print("dir(reflections.keys() ) = {}".format(dir(reflections.keys())))
-        print("reflections.keys().next() = {}".format(reflections.keys().next()))
+        print("dict(reflections.keys()) = {}".format(dict(reflections.keys())))
         for i, experiment in enumerate(experiments):
 
             imageset = experiment.imageset
