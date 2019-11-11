540511443a99814f58c52b10428ea0a54003d040
Jorge Diaz <jorge7soccer@gmail.com>
2019-11-11 13:54:02 -0500

    printing reflections to see location.

--- modules/dials/algorithms/spot_finding/finder.py
+++ modules/dials/algorithms/spot_finding/finder.py
@@ -747,6 +747,7 @@ class SpotFinder(object):
 
         # Loop through all the imagesets and find the strong spots
         reflections = flex.reflection_table()
+        print("reflections = {}".format(reflections))
         print("reflections.keys() = {}".format(reflections.keys()))
         print("dir(reflections.keys() ) = {}".format(dir(reflections.keys())))
         print("dict(reflections.keys()) = {}".format(dict(reflections.keys())))
