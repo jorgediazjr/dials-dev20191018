47627142cabcedd15dc936d6683ccbb706b21f10
Jorge Diaz <jorge7soccer@gmail.com>
2019-11-11 12:08:42 -0500

    Added import sys back into source file.

--- modules/cctbx_project/scitbx/array_family/flex.py
+++ modules/cctbx_project/scitbx/array_family/flex.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 import hashlib
 import six
+import sys
 
 import boost.optional # import dependency
 import boost.std_pair # import dependency
