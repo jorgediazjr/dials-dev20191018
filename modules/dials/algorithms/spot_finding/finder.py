abfc6afafce9c62ebf46a6d0acbfe4ef74e26d81
Jorge Diaz <jorge7soccer@gmail.com>
2019-11-11 13:44:03 -0500

    Printing reflections.keys() in finder.py.

--- modules/dials/algorithms/spot_finding/finder.py
+++ modules/dials/algorithms/spot_finding/finder.py
@@ -747,6 +747,7 @@ class SpotFinder(object):
 
         # Loop through all the imagesets and find the strong spots
         reflections = flex.reflection_table()
+        print("reflections.keys = {}".format(reflections.keys()))
         for i, experiment in enumerate(experiments):
 
             imageset = experiment.imageset
