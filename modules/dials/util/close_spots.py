b1dbbe2a4b32f5cb9df3a55ed95d48c066c6b547
Jorge Diaz <jorge7soccer@gmail.com>
2019-11-13 11:28:41 -0500

    Added -> flex.vec3(0.0, 0.0, 0.0)

--- modules/dials/util/close_spots.py
+++ modules/dials/util/close_spots.py
@@ -131,7 +131,7 @@ def main(reflections):
     for i in closest_pairs:
         print("{:.2f} <=> {:.2f}".format(i, closest_pairs[i][0]))
 
-    reflections['hello'] = (0.0, 0.0, 0.0)
+    reflections['hello'] = flex.vec3(0.0, 0.0, 0.0)
 
     print("reflections.keys = {}".format(reflections.keys()))
     '''
