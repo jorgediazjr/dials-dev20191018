76190e13960eda3ba63a8d059584be473f4c0167
Jorge Diaz <jorge7soccer@gmail.com>
2019-11-27 15:58:34 -0500

    Extracting close spots.

--- modules/dials/util/image_viewer/spotfinder_frame.py
+++ modules/dials/util/image_viewer/spotfinder_frame.py
@@ -1468,19 +1468,16 @@ class SpotFrame(XrayFrame):
                     if 'xy.px.close' in ref_list and self.settings.show_close_spots:
                         self.show_close_spots_timer.start()
                         close_spot = reflection['xy.px.close']
-                        print("reflection = {}".format(reflection))
-                        print("close_spots in ref_list = {}".format(close_spot))
-                        print("Len of close spots = {}".format(len(close_spot)))
                         if close_spot[0] != 0.0 and close_spot[1] != 0.0:
                             print("\nCentroid close spot before: x = {}, y = {}".format(close_spot[0], close_spot[1]))
                             x, y = map_coords(
-                                    close_spot[0], close_spot[1], 0 # ref_list["panel"]
+                                close_spot[0], close_spot[1], 0 # ref_list["panel"]
                             )
                             xm1, ym1 = map_coords(
-                                    close_spot[0] - 1, close_spot[1] - 1, 0 # ref_list["panel"]
+                                close_spot[0] - 1, close_spot[1] - 1, 0 # ref_list["panel"]
                             )
                             xp1, yp1 = map_coords(
-                                    close_spot[0] + 1, close_spot[1] + 1, 0 # ref_list["panel"]
+                                close_spot[0] + 1, close_spot[1] + 1, 0 # ref_list["panel"]
                             )
                             lines = [
                                 (((x, ym1), (x, yp1)), close_spot_dict),
