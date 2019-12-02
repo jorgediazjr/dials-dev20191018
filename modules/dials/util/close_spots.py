3c4a0bdb1346a252b53aac1a8e29ec007da613b2
Jorge Diaz <jorge7soccer@gmail.com>
2019-12-02 13:29:41 -0500

    Lowered the distance to 50.

--- modules/dials/util/close_spots.py
+++ modules/dials/util/close_spots.py
@@ -100,7 +100,7 @@ def euclidean_distance(ordered_points, distance=0.5):
                     dist = find_distance(point_a, point_b)
                     distance = units_distance(point_a, point_b)
                     # print("dist = {}".format(dist)) helpful too see what is the avg value for distance
-                    if dist <= 100:
+                    if dist <= 50:
                         pair = [point_a, point_b]
                         print("{}\t<--\t{:.2f}\t-->\t{}\tWITHIN\t{}".format(point_a, dist, point_b, distance))
                         close_points.append(pair)
