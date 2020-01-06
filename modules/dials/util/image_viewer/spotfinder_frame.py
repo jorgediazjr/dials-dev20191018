08e01790dd85a697989a27f17f3e7d7eb8cea74c
Jorge Diaz <jorge7soccer@gmail.com>
2020-01-06 14:55:00 -0500

    Just printing self.experiments.

--- modules/dials/util/image_viewer/spotfinder_frame.py
+++ modules/dials/util/image_viewer/spotfinder_frame.py
@@ -1493,7 +1493,7 @@ class SpotFrame(XrayFrame):
                 for refl in reflections['xyzobs.px.value']:
                     print("refl = {}".format(refl))
                 
-                print("dict(self.experiments)".format(dict(self.experiments[0])))
+                print("self.experiments".format(self.experiments))
                 reflections.centroid_px_to_mm(self.experiments[0])
 
                 for centroid in closest_points:
