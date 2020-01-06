cbd81e8789e46d9e47877294da5a20a0e9abfae8
Jorge Diaz <jorge7soccer@gmail.com>
2020-01-06 14:39:02 -0500

    Forgot to add the index variable in for loop statement.

--- modules/dials/util/image_viewer/spotfinder_frame.py
+++ modules/dials/util/image_viewer/spotfinder_frame.py
@@ -1480,7 +1480,7 @@ class SpotFrame(XrayFrame):
                 reflections = self.reflections[0]
                 print("reflection object was made = {}".format(reflections))
 
-                for reflection in enumerate(reflections['xyzobs.px.value']):
+                for i, reflection in enumerate(reflections['xyzobs.px.value']):
                     print("reflection {} => {}".format((i+1), reflection))
                     if reflection in closest_points:
                         print("reflection in closest points !!!!!!!")
