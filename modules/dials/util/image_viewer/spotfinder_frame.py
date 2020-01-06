ccca9da7fce7fe160e7435e2e6e5dc2db3687216
Jorge Diaz <jorge7soccer@gmail.com>
2020-01-06 14:22:53 -0500

    Printing the keys of the empty reflection table.

--- modules/dials/util/image_viewer/spotfinder_frame.py
+++ modules/dials/util/image_viewer/spotfinder_frame.py
@@ -1478,6 +1478,8 @@ class SpotFrame(XrayFrame):
 
                 reflections = flex.reflection_table.empty_standard(len(closest_points))
                 print("reflection object was made = {}".format(reflections))
+                for key in dict(reflections):
+                    print("{} => {}".format(key, reflections[key]))
 
                 for centroid in closest_points:
                     x, y = map_coords(
