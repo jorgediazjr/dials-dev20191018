9758e0639113c1a8bd19a9bd80badfc0e7bf9f36
Jorge Diaz <jorge7soccer@gmail.com>
2020-01-06 14:31:45 -0500

    Saving self.reflections to a variable.

--- modules/dials/util/image_viewer/spotfinder_frame.py
+++ modules/dials/util/image_viewer/spotfinder_frame.py
@@ -1476,8 +1476,8 @@ class SpotFrame(XrayFrame):
 
                 closest_points = close_spots.main(reflections_data, dist=10)
 
-                print("type of selected.rows() = {}".format(type(selected.rows())))
-                reflections = flex.reflection_table.empty_standard(len(closest_points))
+                #reflections = flex.reflection_table.empty_standard(len(closest_points))
+                reflections = self.reflections
                 print("reflection object was made = {}".format(reflections))
                 for key in dict(reflections):
                     print("{} => {}".format(key, reflections[key]))
